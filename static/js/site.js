232a03691da1d8020eedbf55d678bea296fdf4c2
guayom <guayo.mena@gmail.com>
2017-12-06 09:22:20 -0600

    Fix problem with js file generation

--- static/js/site.js
+++ static/js/site.js
@@ -1,6 +1,6 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
-  console.log("yeah");
+    console.log("yeah");
     document.getElementById("main-nav").style.transform = "translate3d(0, 0, 0)";
     document.getElementById("main").style.marginLeft = "250px";
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
@@ -13,23 +13,22 @@ function closeNav() {
     document.body.style.backgroundColor = "white";
 }
 
-
 /// SLIDER en el home
 var slides = document.querySelectorAll('#slides .slide');
 var currentSlide = 0;
-var slideInterval = setInterval(nextSlide,4000);
+var slideInterval = setInterval(nextSlide, 4000);
 
 function nextSlide() {
-    goToSlide(currentSlide+1);
+    goToSlide(currentSlide + 1);
 }
 
 function previousSlide() {
-    goToSlide(currentSlide-1);
+    goToSlide(currentSlide - 1);
 }
 
 function goToSlide(n) {
     slides[currentSlide].className = 'slide';
-    currentSlide = (n+slides.length)%slides.length;
+    currentSlide = (n + slides.length) % slides.length;
     slides[currentSlide].className = 'slide showing';
 }
 
@@ -42,26 +41,27 @@ function pauseSlideshow() {
 
 function playSlideshow() {
     playing = true;
-    slideInterval = setInterval(nextSlide,4000);
+    slideInterval = setInterval(nextSlide, 4000);
 }
 
 var next = document.getElementById('next');
 var previous = document.getElementById('previous');
 
-next.onclick = function() {
+next.onclick = function () {
     pauseSlideshow();
     nextSlide();
     playSlideshow();
 };
-previous.onclick = function() {
+previous.onclick = function () {
     pauseSlideshow();
     previousSlide();
     playSlideshow();
 };
-
 
 // Focus al search
 var elm_rows = document.getElementsByClassName("main-search-form")[0];
 elm_rows.addEventListener("click", function (e) {
     this.getElementsByTagName("input")[0].focus();
 });
+
+//# sourceMappingURL=site.js.map
