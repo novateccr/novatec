1b7361aac00db9728923de4c68c650167b5ea593
guayom <guayo.mena@gmail.com>
2018-06-25 04:37:25 -0600

    Add slick slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,5 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
-  console.log("yeah");
     document.getElementById("main-nav").style.transform = "translate3d(0, 0, 0)";
     document.getElementById("main").style.marginLeft = "250px";
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
@@ -13,55 +12,20 @@ function closeNav() {
     document.body.style.backgroundColor = "white";
 }
 
-
-/// SLIDER en el home
-var slides = document.querySelectorAll('#slides .slide');
-var currentSlide = 0;
-var slideInterval = setInterval(nextSlide,4000);
-
-function nextSlide() {
-    goToSlide(currentSlide+1);
-}
-
-function previousSlide() {
-    goToSlide(currentSlide-1);
-}
-
-function goToSlide(n) {
-    slides[currentSlide].className = 'slide';
-    currentSlide = (n+slides.length)%slides.length;
-    slides[currentSlide].className = 'slide showing';
-}
-
-var playing = true;
-
-function pauseSlideshow() {
-    playing = false;
-    clearInterval(slideInterval);
-}
-
-function playSlideshow() {
-    playing = true;
-    slideInterval = setInterval(nextSlide,4000);
-}
-
-var next = document.getElementById('next');
-var previous = document.getElementById('previous');
-
-next.onclick = function() {
-    pauseSlideshow();
-    nextSlide();
-    playSlideshow();
-};
-previous.onclick = function() {
-    pauseSlideshow();
-    previousSlide();
-    playSlideshow();
-};
-
-
 // Focus al search
 var elm_rows = document.getElementsByClassName("main-search-form")[0];
 elm_rows.addEventListener("click", function (e) {
     this.getElementsByTagName("input")[0].focus();
+});
+
+$(document).ready(function(){
+	$('#slides').slick({
+		infinite: true,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 2000,
+		speed: 500,
+		fade: true,
+		cssEase: 'linear'
+	});
 });
