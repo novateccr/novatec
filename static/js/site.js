25ae54bea97c459321a0a2ddb90488a3c618d0a4
Guayo Mena <guayo.mena@gmail.com>
2019-10-13 13:29:07 -0600

    Disable black background

--- static/js/site.js
+++ static/js/site.js
@@ -669,7 +669,6 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
 	document.getElementById("main-nav").classList.add('expanded');
-	document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 	document.body.style.overflow = "hidden";
 }
 
