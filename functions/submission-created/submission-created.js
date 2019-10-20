211d13c5f9c910c0bab153a248200dacbbe82831
Guayo Mena <guayo.mena@gmail.com>
2019-10-20 12:05:03 -0600

    Use axios

--- functions/submission-created/submission-created.js
+++ functions/submission-created/submission-created.js
@@ -1,7 +1,4 @@
-/* eslint-disable */
-// require('dotenv').config()
-
-const https = require("https");
+const axios = require("axios");
 const { GET_RESPONSE_TOKEN } = process.env;
 const lists = [
   {
@@ -10,61 +7,55 @@ const lists = [
     id: `166811701`
   }
 ];
-const formatData = ({ name, email, campaignId, phone, company }) =>
-  JSON.stringify({
-    name,
-    email,
-    campaign: {
-      campaignId
-    },
-    customFieldValues: [
-      {
-        customFieldId: "Vjnecm",
-        value: [phone]
-      },
-      {
-        customFieldId: "VjnJhw",
-        value: [company]
-      },
-      {
-        customFieldId: "VP4wqx",
-        value: ["Website"]
-      }
-    ]
-  });
+// const formatData = ({ name, email, campaignId, phone, company }) =>
+//   {
+//     name,
+//     email,
+//     "campaign": {
+//       campaignId: campaignId
+//     },
+//     customFieldValues: [
+//       {
+//         customFieldId: "Vjnecm",
+//         value: [phone]
+//       },
+//       {
+//         customFieldId: "VjnJhw",
+//         value: [company]
+//       },
+//       {
+//         customFieldId: "VP4wqx",
+//         value: ["Website"]
+//       }
+//     ]
+//   };
 
 exports.handler = async (event, context, callback) => {
   const { nombre, email, tel, empresa } = JSON.parse(event.body);
-  const data = formatData({
+  const data = {
     name: nombre,
     email,
-    campaignId: lists[0].token,
+    campaign: {
+      campaignId: lists[0].token,
+    },
     phone: tel,
     company: empresa
-  });
+  };
 
   const options = {
-    hostname: "api.getresponse.com",
-    path: "/v3/contacts",
-    method: "POST",
     headers: {
       "Content-Type": "application/json",
       "X-Auth-Token": `api-key ${GET_RESPONSE_TOKEN}`
     }
   };
 
-  const req = https.request(options, res => {
-    console.log(`statusCode: ${res.statusCode}`);
-
-    res.on("data", d => {
-      process.stdout.write(d);
-    });
-  });
-
-  req.on("error", error => {
+  try {
+    const response = await axios.post("https://api.getresponse.com/v3/contacts", data, options);
+    console.log("SUCCESS!!!!!")
+    return { statusCode: 200, body: response.statusText };
+  } catch (error) {
+    console.log("FAIL!!!!!");
     console.error(error);
-  });
-
-  req.write(data);
-  req.end();
+    return JSON.stringify({ error: error });
+  }
 };
