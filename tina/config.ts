16938fa11c65a8a5b607c0d3568c53adb4ed6842
Kenneth <kenaguilar.steve@gmail.com>
2023-06-10 18:04:35 -0500

    Add tina config

--- tina/config.ts
+++ tina/config.ts
@@ -13,8 +13,8 @@ const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
 
 export default defineConfig({
   branch,
-  clientId: "8bc04b9b-ec7b-496c-892c-88863c462b64", // Get this from tina.io
-  token: "a75f3e3fbdabe0340d90d1ced3381c5ca1337d66", // Get this from tina.io
+  clientId: "f8f08d39-3226-4975-be3c-a0a852185519", // Get this from tina.io
+  token: "0129b5d9b475207a6e69164a5c3d7d5f2667cda4", // Get this from tina.io
   client: { skip: true },
   build: {
     outputFolder: "admin",
