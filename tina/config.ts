d699c07c9c52ad06a9d061f85deda86a075f7466
Guayo Mena <guayo.mena@gmail.com>
2025-01-15 11:42:13 -0600

    configurar campos de industrias en tina

--- tina/config.ts
+++ tina/config.ts
@@ -1,24 +1,24 @@
-import { defineConfig } from 'tinacms';
-import { acerca_de_nosotrosFields } from './templates';
+import { defineConfig } from "tinacms";
+import { acerca_de_nosotrosFields } from "./templates";
 // import { defaultFields } from './templates';
-import { industriaFields } from './templates';
-import { inicioFields } from './templates';
-import { marcasFields } from './templates';
-import { noticiasFields } from './templates';
-import { productosFields } from './templates';
-import { servicio_t_cnicoFields } from './templates';
+import { industriaFields } from "./templates";
+import { inicioFields } from "./templates";
+import { marcasFields } from "./templates";
+import { noticiasFields } from "./templates";
+import { productosFields } from "./templates";
+import { servicio_t_cnicoFields } from "./templates";
 
 // Your hosting provider likely exposes this as an environment variable
-const branch = process.env.HEAD || 'master';
+const branch = process.env.HEAD || "master";
 
 export default defineConfig({
   branch,
-  clientId: '3691dcce-c73a-4ad9-a39c-742ae6a6f889', // Get this from tina.io
-  token: '9c829a7671fccacf428af1b77c6f1c65982b54ee', // Get this from tina.io
+  clientId: "3691dcce-c73a-4ad9-a39c-742ae6a6f889", // Get this from tina.io
+  token: "9c829a7671fccacf428af1b77c6f1c65982b54ee", // Get this from tina.io
   client: { skip: true },
   build: {
-    outputFolder: 'admin',
-    publicFolder: 'public'
+    outputFolder: "admin",
+    publicFolder: "public",
   },
   media: {
     loadCustomStore: async () => {
@@ -29,208 +29,209 @@ export default defineConfig({
   schema: {
     collections: [
       {
-        format: 'md',
-        label: 'Pages',
-        name: 'pages',
-        path: 'content',
-        frontmatterFormat: 'yaml',
+        format: "md",
+        label: "Pages",
+        name: "pages",
+        path: "content",
+        frontmatterFormat: "yaml",
         match: {
-          include: '*'
+          include: "*",
         },
         templates: [
           {
             fields: [
               {
-                type: 'rich-text',
-                name: 'body',
-                label: 'Body of Document',
-                description: 'This is the markdown body',
-                isBody: true
+                type: "rich-text",
+                name: "body",
+                label: "Body of Document",
+                description: "This is the markdown body",
+                isBody: true,
               },
-              ...acerca_de_nosotrosFields()
+              ...acerca_de_nosotrosFields(),
             ],
-            label: 'acerca-de-nosotros',
-            name: 'acerca_de_nosotros'
+            label: "acerca-de-nosotros",
+            name: "acerca_de_nosotros",
           },
           {
             fields: [
               {
-                type: 'rich-text',
-                name: 'body',
-                label: 'Body of Document',
-                description: 'This is the markdown body',
-                isBody: true
+                type: "rich-text",
+                name: "body",
+                label: "Body of Document",
+                description: "This is the markdown body",
+                isBody: true,
               },
-              ...inicioFields()
+              ...inicioFields(),
             ],
-            label: 'inicio',
-            name: 'inicio'
-          }
-        ]
-      },
-      {
-        format: 'md',
-        label: 'Familias',
-        name: 'familias',
-        path: 'content/familias',
-        frontmatterFormat: 'yaml',
-        match: {
-          include: '**/*'
-        },
-        fields: [
-          {
-            type: 'rich-text',
-            name: 'body',
-            label: 'Body of Document',
-            description: 'This is the markdown body',
-            isBody: true
-          }
-        ]
-      },
-      {
-        format: 'md',
-        label: 'Industrias',
-        name: 'industrias',
-        path: 'content/industrias',
-        frontmatterFormat: 'yaml',
-        match: {
-          include: '**/*'
-        },
-        fields: [
-          {
-            type: 'rich-text',
-            name: 'body',
-            label: 'Body of Document',
-            description: 'This is the markdown body',
-            isBody: true
-          }
-        ]
-      },
-      {
-        format: 'md',
-        label: 'Marcas',
-        name: 'marcas',
-        path: 'content/marcas',
-        frontmatterFormat: 'yaml',
-        match: {
-          include: '**/*'
-        },
-        fields: [
-          {
-            type: 'rich-text',
-            name: 'body',
-            label: 'Body of Document',
-            description: 'This is the markdown body',
-            isBody: true
+            label: "inicio",
+            name: "inicio",
           },
-          ...marcasFields()
-        ]
+        ],
       },
       {
-        format: 'md',
-        label: 'Novatec construccion',
-        name: 'novatec_construccion',
-        path: 'content/novatec-construccion',
-        frontmatterFormat: 'yaml',
+        format: "md",
+        label: "Familias",
+        name: "familias",
+        path: "content/familias",
+        frontmatterFormat: "yaml",
         match: {
-          include: '**/*'
+          include: "**/*",
         },
         fields: [
           {
-            type: 'rich-text',
-            name: 'body',
-            label: 'Body of Document',
-            description: 'This is the markdown body',
-            isBody: true
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true,
           },
-          ...industriaFields()
-        ]
+        ],
       },
       {
-        format: 'md',
-        label: 'Noticias',
-        name: 'noticias',
-        path: 'content/noticias',
-        frontmatterFormat: 'yaml',
+        format: "md",
+        label: "Industrias",
+        name: "industrias",
+        path: "content/industrias",
+        frontmatterFormat: "yaml",
         match: {
-          include: '**/*'
+          include: "**/*",
         },
         fields: [
           {
-            type: 'rich-text',
-            name: 'body',
-            label: 'Body of Document',
-            description: 'This is the markdown body',
-            isBody: true
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true,
           },
-          ...noticiasFields()
-        ]
+          ...industriaFields(),
+        ],
       },
       {
-        format: 'md',
-        label: 'Servicio tecnico',
-        name: 'servicio_tecnico',
-        path: 'content/servicio-tecnico',
-        frontmatterFormat: 'yaml',
+        format: "md",
+        label: "Marcas",
+        name: "marcas",
+        path: "content/marcas",
+        frontmatterFormat: "yaml",
         match: {
-          include: '**/*'
+          include: "**/*",
         },
         fields: [
           {
-            type: 'rich-text',
-            name: 'body',
-            label: 'Body of Document',
-            description: 'This is the markdown body',
-            isBody: true
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true,
           },
-          ...servicio_t_cnicoFields()
-        ]
+          ...marcasFields(),
+        ],
       },
       {
-        format: 'md',
-        label: 'Productos',
-        name: 'productos',
-        path: 'content/productos',
-        frontmatterFormat: 'yaml',
+        format: "md",
+        label: "Novatec construccion",
+        name: "novatec_construccion",
+        path: "content/novatec-construccion",
+        frontmatterFormat: "yaml",
         match: {
-          include: '**/*'
+          include: "**/*",
         },
         fields: [
           {
-            type: 'rich-text',
-            name: 'body',
-            label: 'Body of Document',
-            description: 'This is the markdown body',
-            isBody: true
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true,
           },
-          ...productosFields()
-        ]
+          ...industriaFields(),
+        ],
       },
       {
-        format: 'yaml',
-        label: 'Social Networks',
-        name: 'social_networks',
-        path: 'data',
-        frontmatterFormat: 'yaml',
+        format: "md",
+        label: "Noticias",
+        name: "noticias",
+        path: "content/noticias",
+        frontmatterFormat: "yaml",
+        match: {
+          include: "**/*",
+        },
+        fields: [
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true,
+          },
+          ...noticiasFields(),
+        ],
+      },
+      {
+        format: "md",
+        label: "Servicio tecnico",
+        name: "servicio_tecnico",
+        path: "content/servicio-tecnico",
+        frontmatterFormat: "yaml",
+        match: {
+          include: "**/*",
+        },
+        fields: [
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true,
+          },
+          ...servicio_t_cnicoFields(),
+        ],
+      },
+      {
+        format: "md",
+        label: "Productos",
+        name: "productos",
+        path: "content/productos",
+        frontmatterFormat: "yaml",
+        match: {
+          include: "**/*",
+        },
+        fields: [
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true,
+          },
+          ...productosFields(),
+        ],
+      },
+      {
+        format: "yaml",
+        label: "Social Networks",
+        name: "social_networks",
+        path: "data",
+        frontmatterFormat: "yaml",
         ui: {
           allowedActions: {
             create: false,
-            delete: false
-          }
+            delete: false,
+          },
         },
         match: {
-          include: 'social'
+          include: "social",
         },
         fields: [
           {
-            name: 'dummy',
-            label: 'Dummy field',
-            type: 'string',
+            name: "dummy",
+            label: "Dummy field",
+            type: "string",
             description:
-              'This is a dummy field, please replace it with the fields you want to edit. See https://tina.io/docs/schema/ for more info'
-          }
-        ]
-      }
-    ]
-  }
+              "This is a dummy field, please replace it with the fields you want to edit. See https://tina.io/docs/schema/ for more info",
+          },
+        ],
+      },
+    ],
+  },
 });
